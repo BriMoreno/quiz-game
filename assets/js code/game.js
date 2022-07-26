ccfa6edd663b91ccde0cf526f94f42f5bba283cd
Britania Moreno <69614167+BriMoreno@users.noreply.github.com>
2022-07-25 22:46:49 -0700

    added questions

---
+++ assets/js code/game.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of these is considered Machine Language.",
+    answer: "Binary and Hexadecimal",
+    options: [
+      "Binary",
+      "Beep Boop Beep",
+      "Hypertext Markup Language (HTML)",
+      "Python"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of these is considered Assembly Language.",
+    answer: "Advanced RISC Machine",
+    options: [
+      "Cascading Style Sheet (CSS)",
+      "C++",
+      "Advanced RISC Machine (ARM)",
+      "Java"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the difference between Machine Language and High-Level Language.",
+    answer: "Readablity: Machine language is ment for the machine and High-Level can be read by users then compiled into Machine language",
+    options: [
+      "They are used for different Operating Systems (OS).",
+      "Readablity: Machine language is ment for the machine and High-Level can be read by users then compiled into Machine language",
+      "There is None",
+      "Umm next?"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What type of coding language is Python?",
+    answer: "High-Level and Object-Orientated",
+    options: [
+      "High-Level and Object-Orientated",
+      "A snake's favorite",
+      "Machine",
+      "Scripting"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What was the first programming language called?",
+    answer: "FORTRAN",
+    options: [
+      "SQL",
+      "BASIC",
+      "FORTRAN",
+      "Lisp"
+    ]
+  },
+ 
+    {
+     numb: 6,
+     question: "Is Java and JavaScipt the same?",
+     answer: "They are different coding languages.",
+     options: [
+       "They are the same.",
+       "They are different coding languages.",
+       "Let me ask my mom."
+     ]
+   },
+];
